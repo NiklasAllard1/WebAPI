acc3dcbbcb4f1c9d7bc6c4ecf1a73108141317f6
NiklasAllard1 <143690578+NiklasAllard1@users.noreply.github.com>
2024-01-22 11:48:04 +0100

    Trying to fix bug

--- Program.cs
+++ Program.cs
@@ -1,46 +1,32 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
+var builder = WebApplication.CreateBuilder(args);
 
-class Program
+builder.Services.AddCors(policyBuilder =>
+    policyBuilder.AddDefaultPolicy(policy =>
+        policy.WithOrigins("*").AllowAnyHeader().AllowAnyHeader())
+);
+
+var app = builder.Build();
+app.UseCors();
+
+app.MapGet("/", () => "Hello World!!!");
+
+app.MapGet("/add", (int num1, int num2) => AddNumbers(num1, num2));
+
+app.MapGet("/subtract", (int num1, int num2) => SubtractNumbers(num1, num2));
+
+app.Run();
+
+
+string AddNumbers(int num1, int num2)
 {
-    static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
-
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddMvc();
-
-        var app = builder.Build();
-
-        app.UseHttpsRedirection();
-
-        app.MapGet("/", () => "Hello World!");
-
-        app.MapGet("/add", (HttpContext context) =>
-        {
-            int num1 = int.Parse(context.Request.Query["num1"]);
-            int num2 = int.Parse(context.Request.Query["num2"]);
-            return AddNumbers(num1, num2);
-        });
-
-        app.MapGet("/subtract", (HttpContext context) =>
-        {
-            int num1 = int.Parse(context.Request.Query["num1"]);
-            int num2 = int.Parse(context.Request.Query["num2"]);
-            return SubtractNumbers(num1, num2);
-        });
-
-        app.Run();
-    }
-
-    static string AddNumbers(int num1, int num2)
-    {
-        return $"Summan av {num1} och {num2} är: {num1 + num2}";
-    }
-
-    static string SubtractNumbers(int num1, int num2)
-    {
-        return $"Differensen av {num1} och {num2} är: {num1 - num2}";
-    }
+    return $"Summan av {num1} och {num2} är: {num1 + num2}";
 }
+
+string SubtractNumbers(int num1, int num2)
+{
+    return $"Differensen av {num1} och {num2} är: {num1 - num2}";
+}
+
+
+
+
